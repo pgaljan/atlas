dad972d66a5394053a4a2f933e27926e2c66f96b
shinaakram20 <shinaakram20@gmail.com>
2025-04-22 18:58:38 +0500

    :fire: updates for sprint_seven

--- atlas-nestjs/prisma/migrations/20250411094036_added_thumbnail/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "StructureCatalog" ADD COLUMN     "thumbnail" TEXT;
--- atlas-nestjs/prisma/migrations/20250421101235_add_catalog_constraints/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "CatalogTier" DROP CONSTRAINT "CatalogTier_catalogId_fkey";
-
--- AddForeignKey
-ALTER TABLE "CatalogTier" ADD CONSTRAINT "CatalogTier_catalogId_fkey" FOREIGN KEY ("catalogId") REFERENCES "StructureCatalog"("id") ON DELETE CASCADE ON UPDATE CASCADE;
