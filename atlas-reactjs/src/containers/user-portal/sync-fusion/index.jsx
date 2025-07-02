7dda7854c8b2ac5a93cbdb24bb9b92abaa1ff149
shinaakram20 <shinaakram20@gmail.com>
2025-07-02 22:25:18 +0500

    :fire: fixed issues

--- atlas-reactjs/src/containers/user-portal/sync-fusion/index.jsx
+++ atlas-reactjs/src/containers/user-portal/sync-fusion/index.jsx
@@ -1037,6 +1037,7 @@ const Syncfusion = () => {
           onSuccess={() => {
             handleNodeUpdate();
           }}
+          structureType={structureType}
         />
       )}
       <ConfirmationModal
