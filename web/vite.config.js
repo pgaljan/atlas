d0da2242a7e45c511dab0ad960a11f0e8612c8a6
shinaakram <shinaakram20@gmail.com>
2025-08-28 22:10:26 +0500

    added host and port

--- web/vite.config.js
+++ web/vite.config.js
@@ -21,6 +21,8 @@ export default defineConfig(({ mode }) => ({
     "process.env": {},
   },
   server: {
+    host: process.env.VITE_HOST,
+    port: process.env.VITE_PORT,
     allowedHosts: ['dev.atlasflow.co'],
   },
 }));
